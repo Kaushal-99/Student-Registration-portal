cea1943c7c7b4f3d1fb824c653371921aec5af3f
Makarand <makarandmadhavi99@gmail.com>
2019-07-29 20:25:36 +0530

    added some js

---
+++ js/index.js
@@ -0,0 +1,19 @@
+function view_form() {
+    //console.log("working");
+    $(".content").load("form.html");
+}
+
+function view_status() {
+    //console.log("working");
+    $(".content").load("index.html");
+}
+
+function show_scholarship(){
+    //console.log("show");
+    $("#scholarship").show();
+}
+
+function hide_scholarship(){
+    //console.log("shide");
+    $("#scholarship").hide();
+}
